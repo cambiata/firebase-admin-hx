247d3e0a47b1a45ace5e1639c46612c2ec0441f4
Jonas Nyström <jonasnys@gmail.com>
2018-08-14 00:28:21 +0200

    Functions update

--- src/firebase/auth/Auth.hx
+++ src/firebase/auth/Auth.hx
@@ -5,8 +5,10 @@ import js.Promise;
 
 @:native('admin.auth.Auth') 
 extern class Auth {
+    public var app: firebase.admin.App;
     public function verifyIdToken(idToken:String):Promise<DecodedIdToken>;
     public function getUser(uid:String):Promise<UserRecord>;
+    public function deleteUser(uid:String):Promise<Dynamic>;
     public function createUser(parameters:CreateRequest):Promise<UserRecord>;
     public function getUserByEmail(email:String):Promise<UserRecord>;
     public function getUserByPhoneNumber(phoneNumber:String):Promise<UserRecord>;
--- src/functions/Functions.hx
+++ src/functions/Functions.hx
@@ -1,5 +1,6 @@
 package functions;
 
+import firebase.database.Reference;
 @:native('functions')
 @:jsRequire('firebase-functions')
 extern class Functions {
@@ -7,6 +8,8 @@ extern class Functions {
     static public var https: Https;
 
     static public var database: Database;
+
+    static public var auth: Auth;
 
     static public function config(): Config;
 }
@@ -31,13 +34,62 @@ extern class Database {
     public function ref(path:String): RefBuilder;
 }
 
+
+
 @:native('functions.database.RefBuilder') 
 extern class RefBuilder {
+    public function onCreate(handler:DataSnapshot -> EventContext -> Void):CloudFunction;
+    public function onDelete(handler:DataSnapshot -> EventContext -> Void):CloudFunction;
+    public function onUpdate(handler:Change -> EventContext -> Void):CloudFunction;
+    public function onWrite(handler:Change -> EventContext -> Void):CloudFunction;
+}
+
+@:native('functions.database.DataSnapshot') 
+extern class DataSnapshot {
+    public var key:String;
+    public var ref:Reference;
+    public function child(path:String):DataSnapshot;
+    public function exists():Bool;
+    public function exportVal():Dynamic;
+    public function forEach(action:Dynamic):Bool;
+    public function getPriority(): Dynamic; // getPriority() returns (string, number, or null)
+    public function hasChild(path:String): Bool; //
+    public function hasChildren():Bool;
+    public function numChildren():Int;
+    public function toJSON():Dynamic;
+    public function val():Dynamic;
+}
+
+
+@:native('functions.Change') 
+extern class Change {
+    public var after:DataSnapshot;
+    public var before:DataSnapshot;
+}
+
+@:native('functions.EventContext') 
+extern class EventContext {
+    public var params:Dynamic;
+    public var resource:String;
+    public var timestamp:String;
+}
+
+
+
+@:native('functions.auth') 
+extern class Auth {
+    public function user(): UserBuilder;
+}
+
+@:native('functions.database.UserBuilder') 
+extern class UserBuilder {
     public function onCreate(handler:Event -> Void):CloudFunction;
     public function onDelete(handler:Event -> Void):CloudFunction;
-    public function onUpdate(handler:Event -> Void):CloudFunction;
-    public function onWrite(handler:Event -> Void):CloudFunction;
 }
+
+
+
+
 
 @:native('functions.Event') 
 extern class Event {
